2ca5b5fc9e83c518f7932c371bb91b066ec7cf85
jonah <jonahbeijer@gmail.com>
2026-06-02 20:31:46 +0200

    Railway acces granted

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,8 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
+
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //

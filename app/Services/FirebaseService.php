63fa22df82ccf060bb9e57fd5b55554a9f6bfc44
jonah <jonahbeijer@gmail.com>
2026-06-28 21:44:18 +0200

    FCM

--- app/Services/FirebaseService.php
+++ app/Services/FirebaseService.php
@@ -13,10 +13,24 @@ class FirebaseService
     {
         $client = new Client();
 
-        // DECODEER DE BASE64 STRING UIT JE .ENV
-        $credentials = json_decode(base64_decode(env('FIREBASE_CREDENTIALS_BASE64')), true);
+        // 1. Bouw de credentials array veilig op vanuit de .env bestanden
+        $credentials = [
+            'type' => 'service_account',
+            'project_id' => env('FIREBASE_PROJECT_ID'),
+            // Zorg dat de letterlijke \n tekens worden omgezet naar echte line-breaks
+            'private_key' => str_replace('\\n', "\n", env('FIREBASE_PRIVATE_KEY')),
+            'client_email' => env('FIREBASE_CLIENT_EMAIL'),
+            'client_id' => '100583925862199086313',
+            'auth_uri' => 'https://accounts.google.com/o/oauth2/auth',
+            'token_uri' => 'https://oauth2.googleapis.com/token',
+            'auth_provider_x509_cert_url' => 'https://www.googleapis.com/oauth2/v1/certs',
+            'client_x509_cert_url' => 'https://www.googleapis.com/robot/v1/metadata/x509/' . urlencode(env('FIREBASE_CLIENT_EMAIL')),
+            'universe_domain' => 'googleapis.com',
+        ];
 
+        // 2. Geef de array door aan de Google Client (in plaats van een pad naar een bestand)
         $client->setAuthConfig($credentials);
+
         $client->addScope('https://www.googleapis.com/auth/firebase.messaging');
 
         $token = $client->fetchAccessTokenWithAssertion();
